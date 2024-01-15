84a672b4c65da5124baafe674b7f4a3d51e3b573
AmurKhoyetsyan <42065571+AmurKhoyetsyan@users.noreply.github.com>
2024-01-15 22:24:28 +0400

    Add files via upload

---
+++ hack.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "\n\033[0;32m HACK NASA... \033[0;32m";
+sleep 0.2s
+echo -e "\033[0;32m Starting Hack... \033[0;32m";
+sleep 0.5s
+echo -e "\033[0;32m Hacking NASA 20% \033[0;32m";
+sleep 0.5s
+echo -e "\033[0;32m Hacking NASA 40% \033[0;32m";
+sleep 0.5s
+echo -e "\033[0;32m Hacking NASA 60% \033[0;32m";
+sleep 0.5s
+echo -e "\033[0;32m Hacking NASA 80% \033[0;32m";
+sleep 0.5s
+echo -e "\033[0;32m Hacking NASA 100% \033[0;32m";
+sleep 0.2s
+echo -e "\033[0;32m NASA Hacked Successfull \033[0;32m";
+read -p "Press any key to resume ..."
